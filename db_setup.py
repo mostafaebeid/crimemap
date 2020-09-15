5337d49b071bb1601744d6d6deabfc53eda8674f
mostafaebeid <mostafa.ebeid84@gmail.com>
2020-09-15 13:54:51 +0200

    database setup script

---
+++ db_setup.py
@@ -0,0 +1,26 @@
+import pymysql
+import dbconfig
+
+connection = pymysql.connect(host = 'localhost',
+                             user = dbconfig.db_user,
+                             passwd = dbconfig.db_password)
+
+try:
+    with connection.cursor() as cursor:
+        sql = "CREATE DATABASE IF NOT EXISTS crimemap"
+        cursor.execute(sql)
+        sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
+                id INT NOT NULL AUTO_INCREMENT,
+                latitude DECIMAL (10,6),
+                longitude DECIMAL (10,6),
+                date DATETIME,
+                category VARCHAR(50),
+                description VARCHAR(1000),
+                updated_at TIMESTAMP,
+                PRIMARY KEY (id)    
+        )"""
+        cursor.execute(sql)
+    connection.commit()
+
+finally:
+    connection.close()
