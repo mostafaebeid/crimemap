58ed6c1fbe26305d2b80c81d3db80ff172f86424
mostafaebeid <mostafa.ebeid84@gmail.com>
2020-11-30 19:33:32 +0200

    push origin master

--- crimemap.py
+++ crimemap.py
@@ -1,16 +1,23 @@
 from dbhelper import DBHelper
 from flask import Flask, render_template, request
 import json
+import string
 
 app = Flask(__name__)
 
 DB = DBHelper()
 
+categories = ['Grocery', 'Cleaning Supplies', 'Restaurant']
+
+def sanitize_string(userinput):
+    whitelist = string.ascii_letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
+
 @app.route("/")
 def home():
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes, categories=categories)
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -32,10 +39,15 @@ def clear():
 @app.route("/submitcrime", methods=["POST"])
 def submitcrime():
     category = request.form.get("category")
+    if category not in categories:
+        return home()
     date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
    
